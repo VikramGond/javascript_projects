344813c26f2241613232751803bc6dbbcc0495ed
vikram <vikramgond22052@gmail.com>
2026-07-22 22:44:59 +0530

    add date funtion that fetches date

--- 02-digital-clock/src/script.js
+++ 02-digital-clock/src/script.js
@@ -1,14 +1,25 @@
 let clock = document.getElementById("clock");
+let today = document.getElementById("date");
 
 const updateTime = () => {
-  const time = new Date();
-  let hours = time.getHours().toString().padStart(2,"0");
-  let minutes = time.getMinutes().toString().padStart(2,"0");
-  let seconds = time.getSeconds().toString().padStart(2,0)  ;
+  const now = new Date();
+  let hours = now.getHours().toString().padStart(2, "0");
+  let minutes = now.getMinutes().toString().padStart(2, "0");
+  let seconds = now.getSeconds().toString().padStart(2, 0);
 
   clock.textContent = `${hours}:${minutes}:${seconds}`;
 };
-
 updateTime();
 
-setInterval(updateTime,1000)
+const updateDate = () => {
+  const now = new Date();
+  let date = now.getDate();
+  let month = now.getMonth();
+  let year = now.getFullYear();
+
+  today.textContent = `${date}-${month}-${year}`;
+};
+updateDate();
+
+setInterval(updateTime, 1000);
+
