25c106502a92f63d00b2cb23e7497e3cf60af797
vikram <vikramgond22052@gmail.com>
2026-07-24 12:04:32 +0530

    write the logic for time format

--- 02-digital-clock/src/script.js
+++ 02-digital-clock/src/script.js
@@ -1,14 +1,32 @@
-let clock = document.getElementById("clock");
-let today = document.getElementById("date");
+const clock = document.getElementById("clock");
+const today = document.getElementById("date");
+const selectFormat = document.getElementById("format");
+let format = 12;
+
+selectFormat.addEventListener("change", (event) => {
+  format = event.target.value;
+  updateTime();
+});
+
+const formatter = (value) => {
+  return value.toString().padStart(2, "0");
+};
 
 const updateTime = () => {
   const now = new Date();
-  let hours = now.getHours().toString().padStart(2, "0");
-  let minutes = now.getMinutes().toString().padStart(2, "0");
-  let seconds = now.getSeconds().toString().padStart(2, 0);
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let time = "";
 
-  clock.textContent = `${hours}:${minutes}:${seconds}`;
+  if (format == 12) {
+    time = `${formatter(hours > 12 ? hours - 12 : hours)}:${formatter(minutes)}:${formatter(seconds)} ${hours >= 12 ? "PM" : "AM"}`;
+  } else {
+    time = `${formatter(hours > 12 ? hours - 12 : hours)}:${formatter(minutes)}:${formatter(seconds)} ${hours >= 12 ? "PM" : "AM"}`;
+  }
+  clock.textContent = time;
 };
+
 updateTime();
 
 const updateDate = () => {
@@ -17,9 +35,9 @@ const updateDate = () => {
   let month = now.getMonth();
   let year = now.getFullYear();
 
-  today.textContent = `${date}-${month}-${year}`;
+  today.textContent = `${date}-${(month + 1).toString().padStart(2, "0")}-${year}`;
 };
 updateDate();
 
-setInterval(updateTime, 1000);
 
+setInterval(updateTime, 1000);
